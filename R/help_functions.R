df6787d5a0b4550937bc3262263bc75a483215e4
Inga Schwabe <ingaschwabe@gmail.com>
2017-06-05 21:58:51 +0200

    no message

--- R/help_functions.R
+++ R/help_functions.R
@@ -1,11 +1,20 @@
 #==========================================================
-# help_simulate_twin_data.R
-# Help functions for simulate_twin_data functions
+# help_functions.R
+#
+# Help functions for BayesTwin pacakge
 # Subroutines to calculate probabilites for every response
-# categories under the GPCM and PCM. 
+# categories under the GPCM and PCM, check if response is binary
+#
 # BayesTwin package
 #==========================================================
 
+#For warning in IRT_twin (check if dummy variables need to be used)
+is.categorical = function(x){
+    length(unique(x)) <= 7 #allow for values 0,1 and NA. 
+}
+
+
+#Help functions to simulate data under the GPCM/PCM
 numerator <- function(a, b, theta, k, D=1.7) exp(sum(D*a*(theta-b[1:k])))
 
 denominator <- function(a, b, theta, D=1.7) {
